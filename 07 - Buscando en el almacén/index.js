f190d0cac382dedd3773ed7138eb43267491e209
David Mulero <davicitoo1612@gmail.com>
2021-12-07 21:17:35 +0100

    Añadir resolución del reto #7

---
+++ 07 - Buscando en el almacén/index.js
@@ -0,0 +1,36 @@
+function contains(store, searchProduct) {
+  return (
+    typeof store === "object"
+    ? Object.values(store).some( contenedor => contains(contenedor, searchProduct) )
+    :store === searchProduct
+  )
+}
+
+const almacen = {
+  'estanteria1': {
+    'cajon1': {
+      'producto1': 'coca-cola',
+      'producto2': 'fanta',
+      'producto3': 'sprite'
+    }
+  },
+  'estanteria2': {
+    'cajon1': 'vacio',
+    'cajon2': {
+      'producto1': 'pantalones',
+      'producto2': 'camiseta' // <- ¡Está aquí!
+    }
+  }
+}
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
+    }
+  }
+}
+
+console.log(contains(almacen, 'camiseta')) // true
+console.log(contains(otroAlmacen, 'gameboy')) // false
