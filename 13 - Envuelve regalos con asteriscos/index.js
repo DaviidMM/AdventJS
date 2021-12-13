be7c99126a3c09ae5114ed9fac09848bc87bdcf7
DaviidMM <davicitoo1612@gmail.com>
2021-12-13 19:17:21 +0100

    Reorganizar codigo

--- 13 - Envuelve regalos con asteriscos/index.js
+++ 13 - Envuelve regalos con asteriscos/index.js
@@ -2,7 +2,9 @@ function wrapGifts(gifts) {
   return gifts.reduce((acc, gift, i) => {
     if (gifts.length === 0) return acc
     if (acc.length === 0) acc = [`*${'*'.repeat(gift.length)}*`]
-    return gifts.length === i+1 ? [...acc, `*${gift}*`, `*${'*'.repeat(gift.length)}*`] : [...acc, `*${gift}*`]
+    return gifts.length === i + 1
+      ? [...acc, `*${gift}*`, `*${'*'.repeat(gift.length)}*`]
+      : [...acc, `*${gift}*`]
   }, [])
 }
 
