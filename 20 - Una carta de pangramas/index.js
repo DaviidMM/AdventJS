0e31f0fe24e32ca0e1a6d8287c7484c07ff71ed1
DaviidMM <davicitoo1612@gmail.com>
2021-12-20 17:04:06 +0100

    Añadir solución reto #20

---
+++ 20 - Una carta de pangramas/index.js
@@ -0,0 +1,10 @@
+function pangram(letter) {
+  const abc = "abcdefghijklmnñopqrstuvwxyz".split('')
+  letter = letter.normalize("NFD").normalize().toLowerCase();
+  return abc.every(character => letter.includes(character))
+}
+
+console.log(pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho')) // true
+console.log(pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibe!')) // false
+console.log(pangram('Esto es una frase larga pero no tiene todas las letras del abecedario')) // false
+console.log(pangram('De la a a la z, nos faltan letras')) // false
